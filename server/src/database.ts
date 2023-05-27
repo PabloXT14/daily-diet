4ccaefe19af83257145e49b8f6fbfd19a36ec507
Pablo Alan <pabloxt14@gmail.com>
2023-05-26 23:58:16 -0300

    feat: tring to connected with pg database on WSL

--- server/src/database.ts
+++ server/src/database.ts
@@ -2,7 +2,7 @@ import { knex as setupKnex } from 'knex'
 
 export const knex = setupKnex({
   client: "pg",
-  connection: 'postgresql://postgres:root@localhost:5432/test',
+  connection: 'postgresql://postgres:root@localhost:5433/test',
   searchPath: ['knex', 'public'],
   useNullAsDefault: true,
 })
