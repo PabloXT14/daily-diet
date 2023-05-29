5e3bbe006e805f5a97e7642bc6dbc666dd144b69
Pablo Alan <pabloxt14@gmail.com>
2023-05-29 13:21:56 -0300

    feat: handling env with Zod

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,15 +1,14 @@
 import { Knex, knex as setupKnex } from 'knex'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { env } from './env'
 
 export const config: Knex.Config = {
-  client: process.env.DATABASE_CLIENT,
+  client: env.DATABASE_CLIENT,
   connection:
-    process.env.DATABASE_CLIENT === 'pg'
-      ? process.env.DATABASE_URL
-      : {
-          filename: process.env.DATABASE_URL!,
-        },
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,12 +12,10 @@ app.get('/users', async () => {
     console.log(error)
     return { error }
   }
-
 })
-
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Server is running'))
