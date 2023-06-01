808834792454dff884a867a5d71c07c502414a03
Pablo Alan <pabloxt14@gmail.com>
2023-06-01 14:32:03 -0300

    feat: created meal table type

--- server/src/@types/knex.d.ts
+++ server/src/@types/knex.d.ts
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import { Knex } from 'knex'
 import { UserTableType } from './user-table-type'
+import { MealTableType } from './meal-table-type'
 
 declare module 'knex/types/tables' {
   export interface Tables {
     user: UserTableType
+    meal: MealTableType
   }
 }
---
+++ server/src/@types/meal-table-type.ts
@@ -0,0 +1,11 @@
+export interface MealTableType {
+  id: string
+  name: string
+  description: string
+  meal_date: string
+  meal_time: string
+  is_diet: boolean
+  created_at: string
+  updated_at: string
+  user_id: string
+}
