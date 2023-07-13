3960acb9eb2696fc15ed9e907efb59ff4e4e46ad
Pablo Alan <pabloxt14@gmail.com>
2023-07-13 14:34:54 -0300

    fix: resolving eslint conflict between next config and @rocketseat/eslint-config

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
+import { ReactNode } from 'react'
 
-const nunito_sans = Nunito_Sans({ 
+const nunito_sans = Nunito_Sans({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-nunito-sans',
@@ -13,14 +14,14 @@ export const metadata: Metadata = {
   description: 'Crie uma dieta diária para você',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${nunito_sans.variable} font-sans bg-gray-100 text-gray-600`}>{children}</body>
+      <body
+        className={`${nunito_sans.variable} bg-gray-100 font-sans text-gray-600`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
