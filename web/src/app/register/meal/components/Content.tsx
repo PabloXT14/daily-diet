e145973d147445a2ab5fbef3f8d48059ad77399a
Pablo Alan <pabloxt14@gmail.com>
2023-08-29 17:32:30 -0300

    feat: implemented meal register validation form

--- web/src/app/register/meal/components/Content.tsx
+++ web/src/app/register/meal/components/Content.tsx
@@ -1,64 +1,145 @@
 'use client'
+
 import { useRouter } from 'next/navigation'
-import { FormEvent } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
 import { TextArea } from '@/components/TextArea'
 import { RadioGroup } from '@/components/RadioGroup'
 
+const registerMealFormSchema = z.object({
+  name: z.string().min(3, { message: 'Nome deve ter pelo menos 3 caracteres' }),
+  description: z
+    .string()
+    .min(3, { message: 'Descrição deve ter pelo menos 3 caracteres' }),
+  date: z.string().nonempty({ message: 'Data é obrigatória' }),
+  time: z.string().nonempty({ message: 'Hora é obrigatória' }),
+  isDiet: z
+    .enum(['true', 'false'], {
+      required_error: 'Escolha uma opção',
+    })
+    .transform((isDiet) => {
+      return isDiet === 'true'
+    }),
+})
+
+type RegisterMealFormDataInput = z.input<typeof registerMealFormSchema>
+type RegisterMealFormDataOutput = z.output<typeof registerMealFormSchema>
+
 export function Content() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<RegisterMealFormDataInput, any, RegisterMealFormDataOutput>({
+    resolver: zodResolver(registerMealFormSchema),
+  })
   const router = useRouter()
 
-  async function handleRegisterMeal(event: FormEvent) {
-    event.preventDefault()
+  async function handleRegisterMeal(data: RegisterMealFormDataOutput) {
+    const { name, description, date, time, isDiet } = data
 
-    await router.push(`/register/meal/feedback?is-diet=${true}`)
+    // TODO: Create a new meal in the database
+
+    await router.push(`/register/meal/feedback?is-diet=${isDiet}`)
   }
+
+  console.log(errors)
 
   return (
     <section className="flex-1 overflow-y-auto rounded-t-[20px] bg-gray-100 px-6 py-10">
-      <form className="space-y-6" id="register-meal">
-        <Input label="Nome" />
+      <form
+        className="space-y-6"
+        id="register-meal"
+        onSubmit={handleSubmit(handleRegisterMeal)}
+      >
+        <Input label="Nome" {...register('name')} />
+
+        {errors.name && (
+          <span className="text-xs text-red-500">{errors.name.message}</span>
+        )}
 
         <TextArea
           label="Descrição"
           className="flex h-[120px] resize-none items-start justify-start"
+          {...register('description')}
         />
+        {errors.description && (
+          <span className="text-xs text-red-500">
+            {errors.description.message}
+          </span>
+        )}
 
         <div className="flex w-full gap-5">
-          <Input label="Data" className="w-full flex-1" type="date" />
-          <Input label="Hora" className="w-full flex-1" type="time" />
+          <div className="flex-1">
+            <Input
+              label="Data"
+              className="w-full"
+              type="date"
+              {...register('date')}
+            />
+            {errors.date && (
+              <span className="text-xs text-red-500">
+                {errors.date.message}
+              </span>
+            )}
+          </div>
+
+          <div className="flex-1">
+            <Input
+              label="Hora"
+              className="w-full flex-1"
+              type="time"
+              {...register('time')}
+            />
+            {errors.time && (
+              <span className="text-xs text-red-500">
+                {errors.time.message}
+              </span>
+            )}
+          </div>
         </div>
 
         <div className="flex flex-col gap-2">
           <label className="text-sm font-bold text-gray-600">
             Está dentro da dieta?
           </label>
-          <RadioGroup.Root>
-            <div className="flex w-full gap-2">
-              <RadioGroup.Item value="true" id="option-1" className="w-full">
-                Sim
-              </RadioGroup.Item>
-              <RadioGroup.Item
-                value="false"
-                id="option-2"
-                variant="secondary"
-                className="w-full"
+          <Controller
+            control={control}
+            name="isDiet"
+            render={({ field }) => (
+              <RadioGroup.Root
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                className="flex items-center gap-2"
               >
-                Não
-              </RadioGroup.Item>
-            </div>
-          </RadioGroup.Root>
+                <RadioGroup.Item className="w-full" value="true">
+                  Sim
+                </RadioGroup.Item>
+
+                <RadioGroup.Item
+                  variant="secondary"
+                  className="w-full"
+                  value="false"
+                >
+                  Não
+                </RadioGroup.Item>
+              </RadioGroup.Root>
+            )}
+          />
+          {errors.isDiet && (
+            <span className="text-xs text-red-500">
+              {errors.isDiet.message}
+            </span>
+          )}
         </div>
       </form>
 
-      <Button
-        type="submit"
-        className="mt-10 w-full"
-        form="register-meal"
-        onClick={handleRegisterMeal}
-      >
+      <Button type="submit" className="mt-10 w-full" form="register-meal">
         Cadastrar refeição
       </Button>
     </section>
