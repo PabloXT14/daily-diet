bd7ab934f414ca331de6e6dc9f84a9f98016a14b
Pablo Alan <pabloxt14@gmail.com>
2023-10-26 15:59:44 -0300

    feat: loading user avatar on home page

--- web/src/app/home/components/Header.tsx
+++ web/src/app/home/components/Header.tsx
@@ -1,9 +1,25 @@
+'use client'
+
 import Image from 'next/image'
 import { Avatar } from '@/components/Avatar'
 
 import dailyDietLogoSVG from '@/assets/daily-diet-logo.svg'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/api'
+import { User } from '@/@types/user'
 
 export function Header() {
+  const { data: user } = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const response = await api.get('/users', { withCredentials: true })
+
+      const user = response.data.user as User
+
+      return user
+    },
+  })
+
   return (
     <header className="flex items-center justify-between">
       <Image
@@ -15,8 +31,10 @@ export function Header() {
         className="h-auto w-auto"
       />
       <Avatar.Root className="h-10 w-10 cursor-pointer border-2 border-gray-600">
-        <Avatar.Image src="https://github.com/pabloxt14.png" />
-        <Avatar.Fallback>PA</Avatar.Fallback>
+        <Avatar.Image src={user?.avatar_url} />
+        <Avatar.Fallback>
+          {user?.name.slice(0, 2).toUpperCase()}
+        </Avatar.Fallback>
       </Avatar.Root>
     </header>
   )
