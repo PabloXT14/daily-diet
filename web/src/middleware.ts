d37afcf4d94d9c402f0f05ddb13ccf865fddba8e
Pablo Alan <pabloxt14@gmail.com>
2023-10-30 16:35:49 -0300

    feat: check if token JWT doesn't expires

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,10 +1,39 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { decode } from 'jsonwebtoken'
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
 
   const signinURL = new URL('/signin', request.url)
   const homeURL = new URL('/', request.url)
+
+  try {
+    if (!token) throw new Error('JWT token missing')
+
+    const decoded = decode(token)
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000)
+
+    if (!decoded) throw new Error('Invalid JWT token')
+
+    if (decoded.exp <= currentTimeInSeconds)
+      throw new Error('JWT token expired')
+
+    if (request.nextUrl.pathname === '/signin') {
+      return NextResponse.redirect(homeURL)
+    }
+  } catch (error) {
+    console.log(error)
+
+    if (request.nextUrl.pathname === '/signin') {
+      return NextResponse.next()
+    }
+
+    if (request.nextUrl.pathname === '/register/user') {
+      return NextResponse.next()
+    }
+
+    return NextResponse.redirect(signinURL)
+  }
 
   // User not authenticated
   if (!token) {
